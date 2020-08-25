da6cf532898ee583fe5af3aadf3e6bd965921e1b
Henning <henning.kasch@fh-kiel.de>
2020-08-25 10:07:22 +0200

    Define interface for route params w/ id

--- client/src/hooks/useIdParam.ts
+++ client/src/hooks/useIdParam.ts
@@ -1,8 +1,12 @@
 import { useParams } from 'react-router-dom'
 import { unshorten } from '../services/short-id'
 
+interface IdRouteParams {
+  id: string
+}
+
 const useIdParam = () => {
-  const { id } = useParams()
+  const { id } = useParams<IdRouteParams>()
   if (id === undefined) {
     throw new Error("Path parameter 'id' not found")
   }
